03fbcbdeaee83061d10d57ee14cc9465afdd7977
rafaelanobre <raafanobre@gmail.com>
2023-04-04 17:44:35 -0300

    html e css pronto sem os bônus, início da lógica do js

---
+++ scripts.js
@@ -0,0 +1,40 @@
+function startGame(){
+    //prompt quantas cartas quer jogar
+    //verificar se tem um número de 4 a 14 e se é par
+    //chamar a função que dá as cartas
+    //chamar a função que inicia o cronômetro
+
+
+    //se não tiver
+    //recomeçar essa função
+}
+
+function timer(){
+    //contar os segundos
+    //começa quando a função startGame chama
+    //termina quando começa a função winGame 
+}
+
+function winGame(){
+    //se todas as cartas tiverem pares
+
+    //parar o cronometro
+
+    //exibir alert vitória: Você ganhou em X jogadas. A duração do jogo foi de Y segundos!
+
+    //chamar a função de reiniciar o jogo
+}
+
+function restartGame(){
+    //prompt quer reiniciar o jogo
+
+    //se a reposta for sim
+    //chamar a função startGame
+
+    //se a reposta for não
+    //fechar o prompt e manter as cartas lá
+
+    //se a resposta for quanlquer outra
+    //recomeçar essa função
+}
+
